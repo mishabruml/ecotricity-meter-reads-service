cf41354d9a2f93b3d4330233e7584f70b47e3cc7
Misha Bruml <mishabruml@gmail.com>
2019-08-20 16:29:39 +0100

    trying out basic error handling

--- api/helloworld.js
+++ api/helloworld.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 const nameModel = require("../src/db/models/nameModel");
 
 module.exports = async (req, res) => {
-  console.log(process.env.PROD_DB_URI);
-  await mongoose.connect(process.env.PROD_DB_URI);
-  const { name = "world" } = req.query;
-
-  await nameModel.create({ name });
-  await mongoose.disconnect();
-  res.status(200).send(`Hello ${name}!`);
+  try {
+    await mongoose.connect(process.env.PROD_DB_URI);
+    const { name = "world" } = req.query;
+    await nameModel.create({ name });
+    await mongoose.disconnect();
+    res.status(200).send(`Hello ${name}!`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 };
