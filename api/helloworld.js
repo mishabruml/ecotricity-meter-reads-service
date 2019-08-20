923bdf60c24334bc50b8a1949f4a153d1436732a
Misha Bruml <mishabruml@gmail.com>
2019-08-20 16:16:48 +0100

    created a data schema and model, succesfuly added data to db

--- api/helloworld.js
+++ api/helloworld.js
@@ -1,4 +1,12 @@
+const mongoose = require("mongoose");
+const nameModel = require("../src/db/models/nameModel");
+
 module.exports = async (req, res) => {
+  console.log(process.env.PROD_DB_URI);
+  await mongoose.connect(process.env.PROD_DB_URI);
   const { name = "world" } = req.query;
+
+  await nameModel.create({ name });
+  await mongoose.disconnect();
   res.status(200).send(`Hello ${name}!`);
 };
