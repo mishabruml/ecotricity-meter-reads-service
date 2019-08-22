96815cd9cb1ac0637c8a998b6f17edc3b9ab59ad
Misha Bruml <mishabruml@gmail.com>
2019-08-22 23:41:24 +0100

    remove hello-world and name model

--- src/db/models/nameModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const nameSchema = new mongoose.Schema({
-  name: { type: String, required: true }
-});
-
-const nameModel = mongoose.model("names", nameSchema);
-
-module.exports = nameModel;
