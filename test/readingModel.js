42fd64806d5521c1e498705b7c4a40d7f6f72920
Misha Bruml <mishabruml@gmail.com>
2019-08-22 23:57:15 +0100

    continue unit testing reading model validation

--- test/readingModel.js
+++ test/readingModel.js
@@ -3,20 +3,29 @@ const ReadingModel = require("../src/db/models/readingModel");
 const generateRandomReading = require("./util/generateRandomReading");
 
 describe("reading schema", () => {
+  it("should successfully create an instance of the model with valid data", async () => {
+    const randomReadingData = generateRandomReading();
+    const reading = new ReadingModel(randomReadingData);
+    await reading.validate();
+  });
+
   it("should fail validation when no customerId is specified", async () => {
-    const reading = new ReadingModel();
+    const randomReadingData = generateRandomReading();
+    randomReadingData.customerId = null;
+    const reading = new ReadingModel(randomReadingData);
     reading.validate(err => {
       expect(err.name).to.eql("ValidationError");
       expect(err.errors.customerId.path).to.eql("customerId");
     });
   });
 
-  it("should create an instance of the model with valid data", async () => {
+  it("should fail validation when no serialNumber is specified", async () => {
     const randomReadingData = generateRandomReading();
-
+    randomReadingData.serialNumber = null;
     const reading = new ReadingModel(randomReadingData);
-
-    await reading.validate();
-    console.log(reading);
+    reading.validate(err => {
+      expect(err.name).to.eql("ValidationError");
+      expect(err.errors.serialNumber.path).to.eql("serialNumber");
+    });
   });
 });
