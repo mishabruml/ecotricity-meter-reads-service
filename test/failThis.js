b74e70e69704ef586851563e969ec3cd1798e7ba
Misha Bruml <mishabruml@gmail.com>
2019-08-22 16:49:17 +0100

    fixed the failing test!

--- test/failThis.js
+++ test/failThis.js
@@ -1,7 +1,6 @@
 const assert = require("assert");
 describe("another test", () => {
   it("should fail because im testing the ci", () => {
-    assert.equal(0, 1);
-    // heres some change
+    assert.equal(1, 1);
   });
 });
