91ff4df2e12fa95004e20380c0ecfa165b7f8c91
Misha Bruml <mishabruml@gmail.com>
2019-08-23 00:47:10 +0100

    extract read subdoc schema to a separate schema altogether

--- src/db/models/readingModel.js
+++ src/db/models/readingModel.js
@@ -1,18 +1,22 @@
 const mongoose = require("mongoose");
 
+// "sub" schema for the reads sub-document
+const readSchema = new mongoose.Schema(
+  {
+    type: { type: String, required: true, enum: ["NIGHT", "ANYTIME"] },
+    registerId: { type: String, required: true },
+    value: { type: Number, require: true }
+  },
+  { _id: false } // don't need id for the subdoc
+);
+
+// Main schema
 const readingSchema = new mongoose.Schema({
   customerId: { type: String, required: true, index: true },
   serialNumber: { type: Number, required: true },
   mpxn: { type: String, required: true },
-  read: [
-    // sub-document schema
-    new mongoose.Schema(
-      { type: String, registerId: String, value: String },
-      { _id: false, required: true }
-    )
-  ],
-
+  read: { type: [readSchema], required: true },
   readDate: { type: Date, required: true }
 });
 
-module.exports = mongoose.model("readings", readingSchema);
+module.exports = mongoose.model("readings", readingSchema); // export as model
