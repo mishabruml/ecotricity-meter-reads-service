dd7e3a0a0b60b965b441f46c0b17cb4ded9b5d38
Misha Bruml <mishabruml@gmail.com>
2019-08-22 16:01:54 +0100

    a change for the pr

--- test/failThis.js
+++ test/failThis.js
@@ -2,5 +2,6 @@ const assert = require("assert");
 describe("another test", () => {
   it("should fail because im testing the ci", () => {
     assert.equal(0, 1);
+    // heres some change
   });
 });
