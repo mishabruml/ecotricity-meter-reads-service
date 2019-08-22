1d36a86015928e1a00ff20e743ae9b823d6e9a9e
Misha Bruml <mishabruml@gmail.com>
2019-08-22 19:53:01 +0100

    added db connection and error handling to post method

--- api/meter-read/post.js
+++ api/meter-read/post.js
@@ -1,5 +1,15 @@
+const mongoose = require("mongoose");
+
 module.exports = async (req, res) => {
-  console.log(req.method);
-  console.log(req.body);
-  res.send("posted reading ok");
+  try {
+    await mongoose.connect(process.env.PROD_DB_URI, { useNewUrlParser: true });
+    console.log(req.method);
+    console.log(req.body);
+    res.send("posted reading ok");
+  } catch (e) {
+    console.error(err);
+    res.status(500).send("Server error");
+  } finally {
+    await mongoose.disconnect();
+  }
 };
