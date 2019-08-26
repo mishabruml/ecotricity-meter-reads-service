fe9079b107145fb6493d345e141d1c3761d75fc9
Misha Bruml <mishabruml@gmail.com>
2019-08-26 22:44:23 +0100

    added model creation on POST route! added automation tool for POSTing random data to db. exposed via npm script.

--- test/util/postReading.js
+++
@@ -1,8 +0,0 @@
-const post = require("../../api/meter-read/post");
-const generateRandomReading = require("./generateRandomReading");
-
-const postMockData = async () => {
-  const mockData = generateRandomReading();
-  await post(mockData);
-  return mockData;
-};
