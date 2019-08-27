e9f8680574c4094ee50f8c80c1a56e70137e88c4
Misha Bruml <mishabruml@gmail.com>
2019-08-27 16:07:44 +0100

    rename a variable

--- src/util/validation/validateDataUniqueness.js
+++ src/util/validation/validateDataUniqueness.js
@@ -2,9 +2,9 @@ const { DuplicateError } = require("../../lib/errors");
 const ReadingModelController = require("../../db/controllers/readingModelController");
 const readingModelController = new ReadingModelController();
 
-const validateDataUniqueness = async body => {
-  const existingRecords = await readingModelController.getExactMatches(body);
-  if (existingRecords.length) throw new DuplicateError(body);
+const validateDataUniqueness = async data => {
+  const existingRecords = await readingModelController.getExactMatches(data);
+  if (existingRecords.length) throw new DuplicateError(data);
   return true;
 };
 
