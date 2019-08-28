d95280751ebe7089082ef22a592157f0b241773b
Misha Bruml <mishabruml@gmail.com>
2019-08-28 13:52:50 +0100

    remove only from readingmodelcontroller test, to merge new checks into dev, master ci

--- test/db/readingModelController.js
+++ test/db/readingModelController.js
@@ -2,7 +2,7 @@ const ReadingModelController = require("../../src/db/controllers/readingModelCon
 
 const { expect } = require("chai");
 
-describe.only("ReadingModel db controller class", async () => {
+describe("ReadingModel db controller class", async () => {
   it("should be able to instantiate the class OK", () => {
     const readingModelController = new ReadingModelController();
     expect(readingModelController).to.be.an.instanceOf(ReadingModelController);
