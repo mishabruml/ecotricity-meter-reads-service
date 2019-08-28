7d3efe8816daada60b2092d9f2d0d6e12d936ea6
Misha Bruml <mishabruml@gmail.com>
2019-08-28 14:49:00 +0100

    test the getByIdempotency method

--- test/db/readingModelController.js
+++ test/db/readingModelController.js
@@ -1,10 +1,47 @@
 const ReadingModelController = require("../../src/db/controllers/readingModelController");
+const ReadingModel = require("../../src/db/models/readingModel");
 
 const { expect } = require("chai");
 
-describe("ReadingModel db controller class", async () => {
-  it("should be able to instantiate the class OK", () => {
-    const readingModelController = new ReadingModelController();
+const sinon = require("sinon");
+require("sinon-mongoose");
+const Chance = require("chance");
+const chance = new Chance();
+
+describe.only("ReadingModel db controller class", async () => {
+  let readingModelController;
+
+  beforeEach("instantiate the class", () => {
+    readingModelController = new ReadingModelController();
     expect(readingModelController).to.be.an.instanceOf(ReadingModelController);
+  });
+
+  afterEach("restore the stubs", () => {
+    sinon.restore();
+  });
+
+  it("should get a record from the db by idempotencyKey", async () => {
+    // create some seed data
+    const idempotencyKey = chance.guid();
+    const _id = "fake_mongo_id";
+
+    // Mock (stub) the model native find command to return the seed data
+    const findMock = sinon
+      .mock(ReadingModel)
+      .expects("find")
+      .withArgs({ idempotencyKey }, { idempotencyKey: true, _id: true })
+      .chain("sort")
+      .withArgs({ createdAt: -1 })
+      .chain("lean")
+      .resolves({ idempotencyKey, _id });
+
+    // call the controller methods with the mocked 'find'
+    const result = await readingModelController.getAllByIdempotencyKey(
+      idempotencyKey
+    );
+
+    expect(result).to.deep.eql({ idempotencyKey, _id });
+
+    findMock.restore();
   });
 });
